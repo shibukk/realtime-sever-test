f586419c761056c7b37e774718df54ecec0ec2b2
shibuya kazuaki <kazuaki.shibuya@happyelements.co.jp>
2015-12-17 23:33:45 +0900

    相手にぶつかるとMQが登録される

--- sample.js
+++ sample.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
+var amqp = require('amqp');
+var amqpConn = amqp.createConnection({host: 'localhost', port: 5672});
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -11,11 +13,34 @@ server.listen(port, function () {
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-  socket.on('new message', function (data) {
-    socket.broadcast.emit('new message', {
-      message: data
+  socket.on('login', function (data) {
+    socket.broadcast.emit('login', {
+      id: data.id
     });
-    console.log(data);
+  });
+  socket.on('move', function (data) {
+    socket.broadcast.emit('move', {
+      x: data.x,
+      y: data.y,
+    });
+  });
+  socket.on('on', function (data) {
+    console.log('相手にぶつかりました');
+    amqpConn.publish('node_action', {id: data.id, damage: 1});
   });
   socket.on('disconnect', function(){});
+});
+
+amqpConn.on('error', function(e) {
+    throw e;
+});
+
+amqpConn.on('close', function(e) {
+    console.log('RabbitMQ connection closed.');
+});
+
+amqpConn.on('ready', function() {
+  amqpConn.queue('node_action', {'durable': false}, function(q){
+    console.log("RabbitMQ ready 'node_action'");
+  });
 });
